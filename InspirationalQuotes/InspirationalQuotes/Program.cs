156a9131227a24cc57957ddd8bece3c6174902a3
Walt Beaman <walter.g.beaman@outlook.com>
2023-05-25 20:16:54 -0500

    Initial commit.

---
+++ InspirationalQuotes/InspirationalQuotes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Nodes;
+
+namespace InspirationalQuotes
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Here's your daily motivation:");
+            var quote = await GetDailyQuoteAsync();
+            Console.WriteLine(quote);
+        }
+
+        static async Task<string> GetDailyQuoteAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // API options:
+                //https://zenquotes.io/api/today
+                //https://quotes.rest/qod?category=inspire
+                string apiUrl = "https://zenquotes.io/api/today";
+                string response = await client.GetStringAsync(apiUrl);
+
+                JsonArray jsonArray = JsonNode.Parse(response) as JsonArray;
+                JsonObject quoteObject = jsonArray[0] as JsonObject;
+
+                string quote = quoteObject["q"].ToString();
+
+                return quote;
+            }
+        }
+    }
+}
